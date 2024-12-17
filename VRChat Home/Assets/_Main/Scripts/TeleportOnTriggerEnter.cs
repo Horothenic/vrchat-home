d6faa311b948406d161436c8718fa7fdc55b8451
Ivan Herrera <ivan@vrchat.com>
2024-12-17 11:33:41 -0600

    Added main structure and base Insect environment

---
+++ VRChat Home/Assets/_Main/Scripts/TeleportOnTriggerEnter.cs
@@ -0,0 +1,13 @@
+﻿using UdonSharp;
+using UnityEngine;
+using VRC.SDKBase;
+
+public class TeleportOnTriggerEnter : UdonSharpBehaviour
+{
+    [SerializeField] private Transform teleportTarget;
+
+    public override void OnPlayerTriggerEnter(VRCPlayerApi player)
+    {
+        player.TeleportTo(teleportTarget.position, teleportTarget.rotation);
+    }
+}
